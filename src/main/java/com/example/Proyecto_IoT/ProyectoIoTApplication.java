81a24eb0014f245a43dc8fe03ac2dc8283432745
PvllvKuxal <j.contreras26@ufromail.cl>
2025-07-05 01:08:55 -0400

    feat: subida de archivos iniciales del proyecto

---
+++ src/main/java/com/example/Proyecto_IoT/ProyectoIoTApplication.java
@@ -0,0 +1,14 @@
+package com.example.Proyecto_IoT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoIoTApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoIoTApplication.class, args);
+		System.out.println("Proyecto IoT Application has started successfully.");
+	}
+
+}
---
+++ src/test/java/com/example/Proyecto_IoT/ProyectoIoTApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Proyecto_IoT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoIoTApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
